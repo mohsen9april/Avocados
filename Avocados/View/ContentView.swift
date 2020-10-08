da0a2e3659e9d370c97e6b7afcc853ad67440808
Mohsen Abdollahi <mohsen9april@gmail.com>
2020-10-08 22:17:26 +0330

    Part6 : Add HeaderView and show With Animaiton

--- Avocados/View/ContentView.swift
+++ Avocados/View/ContentView.swift
@@ -10,7 +10,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 20){
+                
+                //MARK:- HEADER
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(alignment: .top, spacing: 0){
+                        HeaderView()
+                    }
+                }
+                
+                //MARK:- FOOTER
+                VStack(alignment: .center, spacing: 20) {
+                    Text("All About Avocados")
+                        .font(.system(.title, design: .serif))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("ColorGreenAdaptive"))
+                        .padding(8)
+                    
+                    Text("Everything you want to know about Avocados but were too afraid to ask!")
+                        .font(.system(.body, design: .serif))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color.gray)
+                }
+                .frame(maxWidth:640)
+                .padding()
+                .padding(.bottom,85)
+            }
+            }.edgesIgnoringSafeArea(.all).padding(0)
     }
 }
 
